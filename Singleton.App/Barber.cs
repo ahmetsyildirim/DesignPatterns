0e460d19c421c574c30ee350b834bb2a92fff8da
Ahmet Yildirim <ahmets.yildirim@gmail.com>
2017-08-08 23:16:57 +1000

    singleton sample created

---
+++ Singleton.App/Barber.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.App
+{
+    public sealed class Barber
+    {
+        private static string id;
+        private static Barber instance;
+        private static object sync = new object();
+
+        private Barber()
+        {
+        }
+
+        public static Barber Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (sync)
+                    {
+                        if (instance == null)
+                        {
+                            //generating unique id for the singleton object.
+                            //Just to be able to test each instance we request has the same id.  
+                            id = Guid.NewGuid().ToString();
+                            instance = new Barber();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public void CutHair()
+        {
+            //printing id to see each instance is same object
+            Console.WriteLine("You haircut is done and my Id is: " + id);
+        }
+    }
+}
--- Singleton.App/Program.cs
+++ Singleton.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace Singleton.App
     {
         static void Main(string[] args)
         {
+            Barber barber1 = Barber.Instance;
+
+            Barber barber2 = Barber.Instance;
+
+            barber1.CutHair();
+            barber2.CutHair();
+
+            Debug.WriteLine(barber1.Id.Equals(barber2.Id));
+
+            Console.ReadKey();
+
+            Console.WriteLine("Switching to thread safe tests...");
+
+            Parallel.For(1, 10, x =>
+            {
+                var barber = Barber.Instance;
+                barber.CutHair();
+            });
+
+            Console.ReadKey();
         }
     }
 }
